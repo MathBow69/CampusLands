6c3256f8ac6f9b8b69aa5b39365997755b7121fb
MathBow69 <156488218+MathBow69@users.noreply.github.com>
2024-01-25 20:04:46 -0500

    security breach xddd

--- codigoEnClase/confgituser.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SETLOCAL EnableDelayedExpansion
-set condition=true
-git config --global user.name "MathBow69" 
-git config --global user.email "156488218+MathBow69@users.noreply.github.com"
-git config --global core.editor "code --wait"
-git config --global init.defaultBranch main
-ENDLOCAL
